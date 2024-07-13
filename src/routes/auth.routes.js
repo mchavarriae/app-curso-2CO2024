de20f53e598bdfebfa57b0597bb35249d4cdef59
Marlon Chavarria <marlonc@tevixmd.com>
2024-07-13 12:07:41 -0600

    Finalizacion Backend - Semana 8

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import {login,register, logout, profile} from "../controllers/auth.controllers.js";
 import { authRequired } from "../middlewares/validateToken.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import {registerSchema, loginSchema} from "../schemas/auth.schema.js";
 
 const router = Router();
 
 //POST localhost:4000/api/register
-router.post('/register',register);
+router.post('/register',validateSchema(registerSchema),register);
 //POST localhost:4000/api/login
-router.post('/login',login);
+router.post('/login',validateSchema(loginSchema), login);
 
 router.post('/logout', logout)
 
