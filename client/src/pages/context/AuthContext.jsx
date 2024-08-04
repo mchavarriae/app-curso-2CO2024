6ceb991b7f93842a345f74c282f66d5fe923d0cd
Marlon Chavarria <marlonc@tevixmd.com>
2024-08-03 19:50:02 -0600

    semana 11 parte 2

--- client/src/pages/context/AuthContext.jsx
+++ client/src/pages/context/AuthContext.jsx
@@ -5,7 +5,7 @@ export const AuthContext = createContext();
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
-    if(!context){
+    if (!context) {
         throw new Error("useAuth must be used within an AuthProvider");
     }
     return context;
@@ -14,16 +14,29 @@ export const useAuth = () => {
 export const AuthProvider = ({ children }) => {
     // el usuario que va a ser leido en toda la aplicación
     const [user, setUser] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [errors, setErrors] = useState([]);
     const signup = async (user) => {
-        const res = await registerRequest(user);
-        console.log(res.data);
-        setUser(res.data);
+        try {
+            const res = await registerRequest(user);
+            setIsAuthenticated(true);
+            console.log(res.data);
+            setUser(res.data);
+            setErrors([]);
+        } catch (error) {
+            console.error(error);
+            setUser(null);
+            setErrors(error.response.data);
+            setIsAuthenticated(false);
+        }
     }
 
     return (
         <AuthContext.Provider value={{
             signup,
-            user
+            user,
+            isAuthenticated,
+            errors
         }}>
             {children}
         </AuthContext.Provider>
